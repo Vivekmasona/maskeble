553bb94d221473eefa1aea1dff5e1d3ecb1df828
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2022-10-30 23:11:20 +0530

    Update bottomBar.js

--- music2/JS/bottomBar.js
+++ music2/JS/bottomBar.js
@@ -4,7 +4,7 @@ const topBtn = document.querySelectorAll('#configs a');
 
 topBtn[0].addEventListener('click', function() {
   if (confirm("The About Page will open a page in a new tab. Continue?")) {
-    window.open("https://github.com/n-ce/ytify#about");
+    window.open("https://www.pahilagana.xyz");
   }
 });
 
